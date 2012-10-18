7f2164038561ed6b29b32db29048cd7d6b4c9382
darkwhispering <hedman.mattias.90@gmail.com>
2012-10-18 15:34:03 +0200

    most of the rewrite done

--- hashimage.php
+++ hashimage.php
@@ -2,28 +2,33 @@
 /*
 Plugin Name: EP Hashimage
 Plugin URI: http://darkwhispering.com/wordpress-plugins
-Description: Display image from a hashtag from twitter or instagram in your template, post/page or widget area using template tag, shortcode or the widget.
+Description: Display image by hashtag from twitter or instagram in your template, post/page or widget area using template tag, shortcode or the widget.
 Author: Mattias Hedman & Peder Fjällström
 Version: 4.0.0
 Author URI: http://darkwhispering.com http://earthpeople.se
 */
 
-class Hashimage {
-
-    function __construct($args = array())
-    {
+if (!$_GET['asyncload']) {
+    function plugin_init() {
         // Set default values
-        add_option('ep_hashimage_async','true');
-        add_option('ep_hashimage_img_display','lightbox');
-        add_option('ep_hashimage_refresh','false');
-        add_option('ep_hashimage_thumb_height',200);
-        add_option('ep_hashimage_thumb_width',200);
-        add_option('ep_hashimage_w_thumb_height',80);
-        add_option('ep_hashimage_w_thumb_width',80);
-        add_option('ep_hashimage_lightbox_height',400);
-        add_option('ep_hashimage_lightbox_width',600);
-        add_option('ep_hashiamge_instagram_client_id','');
+        add_option('ep_hashimage_async','true');                // For faster loading
+        add_option('ep_hashimage_img_display','lightbox');      // Display method of retrived images
+        add_option('ep_hashimage_refresh','false');             // Auto reload search every 15 min
+        add_option('ep_hashimage_network','twitter');           // Network to search in twitter/instagram
+        add_option('ep_hashimage_instagram_client_id',NULL);    // Instagram api client_id
 
+        // Default image sizes
+        $default_img_sizes = array(
+            'thumb_h'           => 200,
+            'thumb_w'           => 200,
+            'widget_thumb_h'    => 80,
+            'widget_thumb_w'    => 80,
+            'lightbox_h'        => 400,
+            'lightbox_w'        => 600
+        );
+        add_option('ep_hashimage_img_sizes',$dedault_img_sizes);  
+
+        // Default twitter search networks
         $default_networks = array(
             'instagram' => 'instagr.am',
             'twitpic'   => 'twitpic',
@@ -34,42 +39,45 @@ class Hashimage {
         );
         add_option('ep_hashimage_networks',$default_networks);
 
+        // Plugin version
+        add_option('ep_hashimage_plugin_version','4.0.0');
+    }
+    add_action('init','plugin_init',10);
+}
+
+class Hashimage {
+
+    function __construct($args = array())
+    {
         // Setup settings array
         $default_settings = array(
-            'hashtag'       => 'unicorn',
-            'limit'         => '5',
-            'network'       => '',
-            'type'          => 'plugin',
-            'output'        => 'html',
-            'networks'      => get_option('ep_hashimage_networks'),
-            'async'         => get_option('ep_hashimage_async'),
-            'img_display'   => get_option('ep_hashimage_img_display'),
-            'refresh'       => get_option('ep_hashimage_refresh'),
-            'img_sizes'     => array(
-                'thumb_h'           => get_option('ep_hashimage_thumb_height'),
-                'thumb_w'           => get_option('ep_hashimage_thumb_width'),
-                'widget_thumb_h'    => get_option('ep_hashimage_w_thumb_height'),
-                'widget_thumb_w'    => get_option('ep_hashimage_w_thumb_width'),
-                'lightbox_h'        => get_option('ep_hashimage_lightbox_height'),
-                'lightbox_w'        => get_option('ep_hashimage_lightbox_width')
-            ),
-            'instagram_client_id'   => get_option('ep_hashimage_instagram_client_id',''),
+            'hashtag'               => 'unicorn',
+            'limit'                 => '5',
+            'type'                  => 'plugin',
+            'output'                => 'html',
+            'network'               => get_option('ep_hashimage_network'),
+            'networks'              => get_option('ep_hashimage_networks'),
+            'async'                 => get_option('ep_hashimage_async'),
+            'img_display'           => get_option('ep_hashimage_img_display'),
+            'refresh'               => get_option('ep_hashimage_refresh'),
+            'img_sizes'             => get_option('ep_hashimage_img_sizes'),
+            'instagram_client_id'   => get_option('ep_hashimage_instagram_client_id')
         );
 
+        // Merge default settings with the new arguments from the user
         $this->settings = wp_parse_args($args, $default_settings);
 
-        $this->twitterUrl = '';
-        $this->instagramUrl = '';
-            
-        // Twitter Search
+        // Twitter Search URL
         $this->twitterUrl = 'http://search.twitter.com/search.json?q=&phrase=&ors=';
-        foreach($this->settings['networks'] as $network) {
-            $this->twitterUrl .= $network.'+';
-        }
-        $this->twitterUrl .= 'lang=all&include_entities=true&rpp=500&tag='.str_replace('#','',$this->settings['hashtag']);
+        
+        // Check what networks within twitter we should use and add to the url
+        $this->twitterUrl .= implode('+',$this->settings['networks']);
 
-        // Instagram Search
-        $this->instagramUrl = 'https://api.instagram.com/v1/tags/'.str_replace('#','',$this->settings['hashtag']).'/media/recent?client_id=eee5f1d268ae4465b5931ce74a2f6ae5';
+        // Add the hashtag 
+        $this->twitterUrl .= '&lang=all&include_entities=true&rpp=500&tag='.str_replace('#','',$this->settings['hashtag']);
+
+        // Instagram API with hashtag and client_id
+        $this->instagramUrl = 'https://api.instagram.com/v1/tags/'.str_replace('#','',$this->settings['hashtag']).'/media/recent?client_id='.$this->settings['instagram_client_id'];
 
         // Do the magic
         $this->_init();
@@ -80,24 +88,28 @@ class Hashimage {
     **/
     private function _init()
     {
+        $twitterjson = '';
+        $instagramjson = '';
+        $image = array();
+
         // Check if we should load this asynct or not
-        if (!isset($_GET['asyncload']) || $this->settings['async'] === 'false') {
-            if (empty($this->settings['network'])) {
+        if (isset($_GET['asyncload']) || $this->settings['async'] === 'false')
+        {
+            if ($this->settings['network']['twitter'])
+            {
                 $twitterjson = json_decode($this->_fetchurl($this->twitterUrl, 600+rand(1,120)));
-                $instagramjson = json_decode($this->_fetchurl($this->instagramUrl, 600+rand(1,120)));
-            } else if ($this->settings['network'] === 'twitter') {
-                $twitterjson = json_decode($this->_fetchurl($this->twitterUrl, 600+rand(1,120)));
-            } else if ($this->settings['network'] === 'instagram') {
+            }
+            if ($this->settings['network']['instagram'])
+            {
                 $instagramjson = json_decode($this->_fetchurl($this->instagramUrl, 600+rand(1,120)));
             }
-        } else if ($this->settings['async'] === 'true' && $_GET['asyncload'] === 'true') {
-            $twitterjson = '';
-            $instagramjson = '';
         }
 
         // Process the result from twitter
         if (isset($twitterjson) && $twitterjson->results) {
             foreach ($twitterjson->results as $results) {
+
+                // If it is links to other networks
                 if (isset($results->entities) && isset($results->entities->urls)) {
                     foreach ($results->entities->urls as $url) {
                         if (!empty($url->expanded_url) && !empty($url->url)) {
@@ -106,6 +118,8 @@ class Hashimage {
                         }
                     }
                 }
+
+                // If it is twitter media
                 if (isset($results->entities) && isset($results->entities->media)) {
                     foreach ($results->entities->media as $image) {
                         if (!empty($image->media_url) && !empty($image->url)) {
@@ -117,7 +131,11 @@ class Hashimage {
             }
 
             // Get the images from the links on twitter
-            $images = array_merge($this->_extractimages($links),$images);
+            if ($links && $images) {
+                $images = array_merge($this->_extractimages($links),$images);
+            } else if (!$images) {
+                $image = $links;
+            }
         }
 
         // Process the results from instagram
@@ -125,20 +143,30 @@ class Hashimage {
             foreach ($instagramjson->data as $result) {
                 if (!empty($result->link) && !empty($result->images->standard_resolution->url)) {
                     $images[md5($result->id)]['img'] = $result->images->standard_resolution->url;
-                    $images[md5($result->id)]['link'] = $result->link;
+                    $images[md5($result->id)]['source'] = $result->link;
                 }
             }
         }
-
-        // print_r($this->links);
         
-        $images = array_map('unserialize', array_unique(array_map('serialize', $images)));
+        if ($images) {
+            // Remove any doubles
+            $images = array_map('unserialize', array_unique(array_map('serialize', $images)));
 
-        if (empty($network)) {
-            shuffle($images);
+            // Remove images without img url or source url
+            foreach ($images as $key => $image) {
+                if(empty($image['img']) || empty($image['source'])) {
+                    unset($images[$key]);
+                }
+            }
+
+            // If both twitter and instagram is used, lets shuffel the results
+            if ($this->settings['network']['twitter'] && $this->settings['network']['instagram']) {
+                shuffle($images);
+            }
+
+            // Limit the amount of images returned to the output after the limit
+            $images = array_slice($images, 0, $this->settings['limit']);
         }
-
-        $images = array_slice($images, 0, $this->settings['limit']);
 
         // Build the output
         if ($this->settings['output'] === 'html') {
@@ -183,19 +211,33 @@ class Hashimage {
     private function _extractimages($links){
         if($links){
             foreach($links as $link){
-                if (stristr($link['img'],'yfrog.com')) {
+                // yfrog.com
+                if (stristr($link['img'],'yfrog.com'))
+                {
                     $images[md5($link['img'])]['img'] = $this->_extractyfrog($link['img']);
                     $images[md5($link['img'])]['source'] = $link['source'];
-                } else if (stristr($link['img'],'plixi.com')) {
+                }
+                // plixi.com
+                else if (stristr($link['img'],'plixi.com'))
+                {
                     $images[md5($link['img'])]['img'] = $this->_extractplixi($link['img']);
                     $images[md5($link['img'])]['source'] = $link['source'];
-                } else if (stristr($link['img'],'instagr.am')) {
+                }
+                // instagr.am
+                else if (stristr($link['img'],'instagr.am'))
+                {
                     $images[md5($link['img'])]['img'] = $this->_extractinstagram($link['img']);
                     $images[md5($link['img'])]['source'] = $link['source'];
-                } else if (stristr($link['img'],'twitpic.com')) {
+                }
+                // twitpic.com
+                else if (stristr($link['img'],'twitpic.com'))
+                {
                     $images[md5($link['img'])]['img'] = $this->_extracttwitpic($link['img']);
                     $images[md5($link['img'])]['source'] = $link['source'];
-                } else if (stristr($link['img'],'flic.kr')) {
+                }
+                // flic.kr
+                else if (stristr($link['img'],'flic.kr'))
+                {
                     $images[md5($link['img'])]['img'] = $this->_extractflickr($link['img']);
                     $images[md5($link['img'])]['source'] = $link['source'];
                 }
@@ -271,44 +313,63 @@ class Hashimage {
     **/
     private function _formathtml($images = array())
     {
-        print_r($this->settings);
         $html = '';
 
+        // Arguments for the async loding
         $jsargs = array(
+            'pluginpath'    => plugins_url('hashimage.php',__FILE__),
             'hashtag'       => $this->settings['hashtag'],
             'limit'         => $this->settings['limit'],
-            'async'         => $this->settings['async'],
-            'pluginpath'    => plugins_url('hashimage.php',__FILE__),
             'type'          => $this->settings['type'],
-            'img_display'   => $this->settings['img_display'],
-            'refresh'       => $this->settings['refresh']
+            'async'         => $this->settings['async']
         );
 
-        if ($_GET['asyncload'] != 'true') {
+        // If async is true, add the async arguments as json in a data attribute and display a loading img and text.        
+        if ($this->settings['async'] === 'true' && $_GET['asyncload'] != 'true')
+        {
             $html .= "<ul class='hashimage-container' data-options='".json_encode($jsargs)."'>";
-        }
-        
-        if ($this->async === 'true' && $_GET['asyncload'] != 'true') {
             $html .= '<p><img src="'.plugins_url('loading.gif',__FILE__).'" alt="Loading"> Loading hashimages...</p>';
-        } else {
-            foreach ($images as $image) {
-                if (!empty($image['img'])) {
+        }
+        // If async is false or this is the actual async request, build the html
+        else
+        {
+            $html .= "<ul class='hashimage-container' data-options='".json_encode($jsargs)."'>";
+            if (!empty($images)) {
+                foreach ($images as $image) {
                     $html .= '<li>';
-                    if ($this->settings['img_display'] === 'lightbox') {
-                        if ($this->settings['type'] == 'widget' || $_GET['type'] == 'widget') {
+
+                    // If image display method is lightbox
+                    if ($this->settings['img_display'] === 'lightbox')
+                    {
+                        // If widget
+                        if ($this->settings['type'] == 'widget' || $_GET['type'] == 'widget')
+                        {
                             $html .= '<a href="'.plugins_url('timthumb.php',__FILE__).'?src='.$image['img'].'&amp;w='.$this->settings['img_sizes']['lightbox_w'].'&amp;h='.$this->settings['img_sizes']['lightbox_h'].'&amp;zc=2" rel="lightbox"><img src="'.plugins_url('timthumb.php',__FILE__).'?src='.$image['img'].'&amp;w='.$this->settings['img_sizes']['widget_thumb_w'].'&amp;h='.$this->settings['img_sizes']['widget_thumb_h'].'" alt="Image loaded with Hashimage" /></a>'."\n";
-                        } else {
+                        }
+                        // If not widget
+                        else
+                        {
                             $html .= '<a href="'.plugins_url('timthumb.php',__FILE__).'?src='.$image['img'].'&amp;w='.$this->settings['img_sizes']['lightbox_w'].'&amp;h='.$this->settings['img_sizes']['lightbox_h'].'&amp;zc=2" rel="lightbox"><img src="'.plugins_url('timthumb.php',__FILE__).'?src='.$image['img'].'&amp;w='.$this->settings['img_sizes']['thumb_w'].'&amp;h='.$this->settings['img_sizes']['thumb_h'].'" alt="Image loaded with Hashimage" /></a>'."\n";
                         }
-                    } elseif($this->settings['img_display'] === 'source') {
-                        if ($this->settings['type'] == 'widget' || $_GET['type'] == 'widget') {
+                    }
+                    // If image display method is original source
+                    elseif($this->settings['img_display'] === 'source')
+                    {
+                        // If widget
+                        if ($this->settings['type'] == 'widget' || $_GET['type'] == 'widget')
+                        {
                             $html .= '<a href="'.$image['source'].'" target="_blank"><img src="'.plugins_url('timthumb.php',__FILE__).'?src='.$image['img'].'&amp;w='.$this->settings['img_sizes']['widget_thumb_w'].'&amp;h='.$this->settings['img_sizes']['widget_thumb_h'].'" alt="Image loaded with Hashimage" /></a>'."\n";
-                        } else {
+                        }
+                        // If not widget
+                        else
+                        {
                             $html .= '<a href="'.$image['source'].'" target="_blank"><img src="'.plugins_url('timthumb.php',__FILE__).'?src='.$image['img'].'&amp;w='.$this->settings['img_sizes']['thumb_w'].'&amp;h='.$this->settings['img_sizes']['thumb_h'].'" alt="Image loaded with Hashimage" /></a>'."\n";
                         }
                     }
                     $html .= '</li>';
-                }                
+                }  
+            } else {
+                $html .= '<li>No images found for your hashtag!</li>';
             }
         }
         $html .= '</ul>';
@@ -321,3 +382,44 @@ function hashimage($args = array()){
     $hashimage = new Hashimage($args);
     return $hashimage->output;
 }
+
+/**
+* IF THIS IS AN ASYNC REQUEST
+**/
+if(!defined('ABSPATH')){
+    require("../../../wp-load.php");
+    $args = array (
+        'hashtag'   => strip_tags($_GET['hashtag']),
+        'limit'     => (int)$_GET['limit'],
+        'type'      => $_GET['type']
+    );
+    echo hashimage($args); 
+    exit(0);
+}
+
+// Include widget
+include_once('widget.php');
+
+// Include shortcode
+include_once('shortcode.php');
+
+// Include settings page
+include_once('settings.php');
+
+//Frontpage JS and CSS
+function hashimage_js() {
+    wp_enqueue_script("jquery");
+    wp_register_script('hashimage_js', plugins_url('js/slimbox2.js', __FILE__));
+    wp_register_script('hashimage_js_async', plugins_url('js/async.js', __FILE__));
+    wp_enqueue_script('hashimage_js');
+    wp_enqueue_script('hashimage_js_async');
+}
+add_action('wp_print_scripts','hashimage_js');
+
+function hashimage_css() {
+    wp_register_style('hashimage_slimbox_css', plugins_url('css/slimbox2.css', __FILE__));
+    wp_register_style('hashimage_css', plugins_url('css/style.css', __FILE__));
+    wp_enqueue_style('hashimage_slimbox_css');
+    wp_enqueue_style('hashimage_css');
+}
+add_action('wp_print_styles','hashimage_css');
